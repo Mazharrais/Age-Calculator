a04d606c8a277ff4cc002e293c6febb00a5d44d8
Mazharrais <mazharmajo72@gmail.com>
2024-07-24 16:43:52 +0500

    add code

--- app.js
+++ app.js
@@ -30,8 +30,20 @@ function calculateAge(){
     if(d2 >= d1){
         d3 = d2 - d1;
     } else {
-        
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 -d1;
+    }
+    if(m3 < 0){
+        m3 = 11;
+        y3--;
     }
 
+
+}
+
+
+function getDaysInMonth(year, month){
+     return new Date(year, month, 0).getDate();
+      
 
 }
